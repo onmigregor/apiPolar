a1233d48aebc4e0ffdce2da0866843ad2c16a856
Omar Ramirez <onmigregor@gmail.com>
2026-03-24 10:14:14 -0400

    Vista productos

--- resources/views/livewire/promotion-details-table.blade.php
+++ resources/views/livewire/promotion-details-table.blade.php
@@ -1,3 +1,10 @@
-<div>
+<div class="relative">
+    <div wire:loading.delay class="absolute inset-0 z-10 flex items-center justify-center rounded-xl bg-white/60 backdrop-blur-sm dark:bg-gray-900/60">
+        <div class="flex flex-col items-center gap-2">
+            <x-filament::loading-indicator class="h-10 w-10 text-primary-500" />
+            <span class="text-sm font-medium text-primary-600 dark:text-primary-400">Procesando...</span>
+        </div>
+    </div>
+    
     {{ $this->table }}
 </div>
